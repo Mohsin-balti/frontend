2b3389f9dd57ce468d8f8032dbc9aab46135d524
Mohsin-balti <59974279+Mohsin-balti@users.noreply.github.com>
2021-03-20 21:40:30 +0500

    Update Hello.js

--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./Hello.css"
 function Hello({fn}) {
-  return <p className="myname">THis is in P tag:{fn}</p>
+  return <p className="myname">THis is in Pppp tag:{fn}</p>
  
 }
 export default Hello;
